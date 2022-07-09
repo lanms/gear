e332e1aa179536e6a05283a8184d63c168b00392
github-action-benchmark <github@users.noreply.github.com>
2022-07-09 13:28:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f91103ace8408ed40a162bc8897bbc2175e0aef1

---
+++ performance-charts/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1657373335532,
+  "repoUrl": "https://github.com/gear-tech/gear",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Arsenii Lyashenko",
+            "username": "ark0f",
+            "email": "arsenylyashenko.3@gmail.com"
+          },
+          "committer": {
+            "name": "Arsenii Lyashenko",
+            "username": "ark0f",
+            "email": "arsenylyashenko.3@gmail.com"
+          },
+          "id": "f91103ace8408ed40a162bc8897bbc2175e0aef1",
+          "message": "Don't use external file because it's unneeded",
+          "timestamp": "2022-07-09T13:17:10Z",
+          "url": "https://github.com/gear-tech/gear/commit/f91103ace8408ed40a162bc8897bbc2175e0aef1"
+        },
+        "date": 1657373334552,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total time",
+            "value": 128838734471,
+            "unit": "ns",
+            "range": "± 881624391",
+            "extra": "Test suites"
+          }
+        ]
+      }
+    ]
+  }
+}
